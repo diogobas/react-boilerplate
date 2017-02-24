fa73757763e116b2ba1a42f2fa5b314fd74489e9
Diogo Bastos <diogo.bastos@neovation.local>
2017-02-24 09:28:42 -0600

    Taking off the require foundation css of the app.jsx file

--- app/app.jsx
+++ app/app.jsx
@@ -3,7 +3,6 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 //Load foundation
-require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
 
 //App class
